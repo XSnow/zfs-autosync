a54ea7e5d227f5c0f316afe38ee5e03f8d185b14
xsnow <xsnow@outlook.com>
2020-07-27 14:03:09 +0800

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
-use std::process::Command;
-
+use std::process::{Command, Stdio};
 
 fn call_cmd(cmd_exec: &str, cmd_line: &[&str]) -> Result<String, String> {
     println!("Calling {} with {:?}", cmd_exec, cmd_line);
@@ -19,20 +18,68 @@ fn call_cmd(cmd_exec: &str, cmd_line: &[&str]) -> Result<String, String> {
     return ret;
 }
 
+fn call_cmd_piped(cmd1_exec: &str, cmd1_line: &[&str], cmd2_exec: &str, cmd2_line: &[&str])
+                  -> Result<String, String> {
+
+    println!("Calling {} with {:?} piped to {} with {:?}", cmd1_exec, cmd1_line, cmd2_exec, cmd2_line);
+
+    let cmd1 = Command::new(cmd1_exec)
+        .args(cmd1_line)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect(&format!("Execute {} command with {:?} as 1st part of piped failed", cmd1_exec, cmd1_line));
+
+    let out1 = cmd1.stdout.unwrap();
+
+    let cmd2 = Command::new(cmd2_exec)
+        .args(cmd2_line)
+        .stdin(out1)
+        .output()
+        .expect(&format!("Execute {} command with {:?} as 2st part of piped failed", cmd2_exec, cmd2_line));
+
+    //cmd1.wait().unwrap();
+
+    let ret = if cmd2.status.success() {
+        Ok(String::from_utf8(cmd2.stdout).unwrap())
+    }else{
+        Err(String::from_utf8(cmd2.stderr).unwrap())
+    };
+
+    return ret;
+
+}
+
 trait ZfsThing {
     const TYPE_STR: &'static str;
     fn fromfs(fsname: &str, name: &str) -> Self;
     fn name(&self) -> &str;
     fn fsname(&self) -> &str;
-    fn list<T: ZfsThing>(&self)  -> Result<Vec<T>, String> {
+    fn list<T: ZfsThing>(&self, childname: Option<&str>)
+                         -> Result<Vec<T>, String> {
+
+        let appendname = childname.map(|s| format!("{}#{}",self.name(), s));
+        let unpackname = appendname.as_ref().map_or(self.name(), |s| s.as_ref());
+
         let v = call_cmd("zfs", &["list", "-t", T::TYPE_STR, "-H", "-o", "name",
-                                  "-p", "-S", "creation", self.name()])?;
+                                  "-p", "-S", "creation", unpackname])?;
 
         Ok(v.lines().map(|v| T::fromfs(self.fsname(), v)).collect())
     }
+
     fn destroy(&self) -> Result<(), String> {
         call_cmd("zfs", &["destroy", self.name()])?;
         Ok(())
+    }
+}
+
+trait ZfsBookmarkable : ZfsThing {
+    fn bookmark(&self, bookmark: &str) -> Result<Bookmark, String> {
+        let full_bookmark = &format!("{}#{}", self.fsname(), bookmark);
+        call_cmd("zfs", &["bookmark", self.name(), full_bookmark])?;
+        Ok( Bookmark {
+            fsname: self.fsname().to_string(),
+            name: full_bookmark.to_string()
+        } )
     }
 }
 
@@ -60,6 +107,7 @@ impl ZfsThing for Bookmark {
     fn name(&self) -> &str { &self.name }
     fn fsname(&self) -> &str { &self.fsname }
 }
+impl ZfsBookmarkable for Bookmark { }
 impl ZfsThing for Snapshot {
     const TYPE_STR: &'static str = "snapshot";
     fn fromfs(fsname: &str, name: &str) -> Self {
@@ -68,10 +116,16 @@ impl ZfsThing for Snapshot {
     fn name(&self) -> &str { &self.name }
     fn fsname(&self) -> &str { &self.fsname }
 }
+impl ZfsBookmarkable for Snapshot { }
 impl Snapshot {
-    fn bookmark(&self, bookmark: &str) -> Result<(), String> {
-        let full_bookmark = &format!("{}#{}", self.fsname(), bookmark);
-        call_cmd("zfs", &["bookmark", self.name(), full_bookmark])?;
+    fn send(&self, bookmark : Option<&Bookmark>, dest : &Filesystem) -> Result<(), String> {
+        let args = bookmark.map_or(
+            vec!["send", self.name()],
+            |b| vec!["send", "-i", b.name(), self.name()]
+        );
+        call_cmd_piped(
+            "zfs", &args,
+            "zfs", &["receive", dest.name()])?;
         Ok(())
     }
 }
@@ -87,17 +141,44 @@ fn main() {
 
     println!("Backup from {} to {}", source.name, dest.name());
 
-    match &source.list::<Filesystem>().unwrap()[..] {
+    match &source.list::<Filesystem>(None).unwrap()[..] {
         [_] =>(), _ => panic!("WTF! IMPOSSIBLE!"),
     }
 
-    let snapshots: Vec<Snapshot> = source.list().unwrap();
+    let snapshots: Vec<Snapshot> = source.list(None).unwrap();
+    let src_s = &snapshots[0];
     println!("Latest Snapshot is {} of {}",
-             snapshots[0].name, snapshots[0].fsname);
+             src_s.name, src_s.fsname);
 
-    let bookmarks: Vec<Bookmark> = source.list().unwrap();
-    // println!("Bookmarks {?:}", )
-    //destroy_bookmark(source, "latest_new");
-    //create_bookmark(source, latest_snapshot, "latest_new");
+    const NEW_BM: &str = "latest_new";
+    match source.list::<Bookmark>(Some(NEW_BM)) {
+        Ok(vec) => if let [s] = &vec[..] {
+            s.destroy().expect("Destroy old bookmark fails");
+        } else {
+            panic!("Zero or more than one bookmark with name {}", NEW_BM);
+        }
+        _ => println!("No {} bookmark detected", NEW_BM),
+    };
 
+    let _new_bookmark = src_s.bookmark(NEW_BM).expect("Create new bookmark fails");
+
+    const OLD_BM: &str = "latest";
+
+    match source.list::<Bookmark>(Some(OLD_BM)) {
+        Ok(bookmarks) => if let [src_i] = &bookmarks[..] {
+            src_s.send(Some(&src_i), &dest).expect("Failed to send");
+            println!("Successfully sent");
+            src_i.destroy().expect("Destroy old bookmark fails");
+        }else {
+            panic!("Zero or more than one bookmark with name {}", OLD_BM);
+        }
+        _ => {
+            src_s.send(None, &dest).expect("Failed to send");
+            println!("Successfully sent");
+        }
+    };
+
+    src_s.bookmark(OLD_BM).expect("Failed to create bookmark");
+
+    // println!("grep: {}", std::str::from_utf8(&cmd2.stdout).unwrap());
 }
